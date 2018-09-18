d02167deeb675fa7675e055f9f43dd201338772f
Felipe Vilella <felipevilellaalves@hotmail.com>
2018-09-17 22:17:46 -0300

    configuração do ambiente de desenvolvimento concluido.

---
+++ application/views/estrutura/footer.php
@@ -0,0 +1,17 @@
+  <script src="<?php echo base_url('assets/bootstrap/js/core/jquery.min.js');?>" type="text/javascript"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/core/popper.min.js');?>" type="text/javascript"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/core/bootstrap-material-design.min.js');?>" type="text/javascript"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/plugins/perfect-scrollbar.jquery.min.js')?>"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/plugins/chartist.min.js')?>"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/plugins/bootstrap-notify.js');?>"></script>
+  <script src="<?php echo base_url('assets/bootstrap/js/material-dashboard.min.js?v=2.1.0');?>" type="text/javascript"></script>
+  <script>
+    $(document).ready(function() {
+      md.initDashboardPageCharts();
+
+    });
+  </script>
+</body>
+
+</html>
---
+++ application/views/estrutura/header.php
@@ -0,0 +1,12 @@
+<doctype html>
+  <html lang="pt-br">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <link rel="stylesheet" type="text/css" href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons'?>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <link href="<?php echo base_url('assets/bootstrap/css/material-dashboard.css?v=2.1.0');?>" rel="stylesheet" />
+  </head>
