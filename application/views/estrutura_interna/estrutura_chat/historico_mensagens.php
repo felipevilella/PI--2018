9e3abaeb136e721f96f266796f37fadb04935de6
AntonioGoncalves96 <37987590+AntonioGoncalves96@users.noreply.github.com>
2018-11-19 16:14:47 -0200

    Conteudo final Peets meeting , não realizar o download

---
+++ application/views/estrutura_interna/estrutura_chat/historico_mensagens.php
@@ -0,0 +1,27 @@
+<div class="chat_list active_chat">
+              <div class="chat_people">
+                <div class="chat_img fotocompleto"> <img src="<?php echo ('assets/personalizado/imagem/avatar.jpg');?>" > </div>
+                <div class="chat_ib">
+                  <h5>Mirian <span class="chat_date">Out 10</span></h5>
+                  <p> Sim e vc?</p>
+                </div>
+              </div>
+            </div>
+            <div class="chat_list">
+              <div class="chat_people">
+                <div class="chat_img fotocompleto"> <img src="<?php echo ('assets/personalizado/imagem/card-profile2-square.jpg');?>" > </div>
+                <div class="chat_ib">
+                  <h5>Pedro <span class="chat_date">Out 19</span></h5>
+                  <p>Vamos caminhar juntos e o Thor esta com saudade da Mel .</p>
+                </div>
+              </div>
+            </div>
+            <div class="chat_list">
+              <div class="chat_people">
+                <div class="chat_img fotocompleto"> <img src="<?php echo ('assets/personalizado/imagem/avatar.jpg');?>" > </div>
+                <div class="chat_ib">
+                  <h5>Bianca <span class="chat_date">Out 21</span></h5>
+                  <p>Qual o endereço da clinica veterinaria que você levou a Mel ?</p>
+                </div>
+              </div>
+</div>
